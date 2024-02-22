bdf7c40fc6daad4f50f8170fc4e3efefe829148c
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-02-21 21:53:00 -0300

    mejoras en abstract form group

--- src/app/models/core/abstract-form-group.model.ts
+++ src/app/models/core/abstract-form-group.model.ts
@@ -1,7 +1,9 @@
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, FormRecord } from '@angular/forms';
 
 export type AbstractFormGroup<T extends Record<string, any>> = FormGroup<{
   [Property in keyof T]:
     | FormControl<T[Property] | undefined>
+    | FormRecord<T[Property]>
+    | FormArray<FormControl<T[Property][0]> | AbstractFormGroup<T[Property][0]>>
     | AbstractFormGroup<T[Property]>;
 }>;
