4ca95e415c78aa17cab83973547e70629536e6a9
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-02-25 12:29:50 -0300

    ordenar member types en el dataservice

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -22,52 +22,6 @@ import {
 })
 export class DatabaseService<T> {
   private readonly _getTableReply$ = new Subject<RequestTableResponse<T>>();
-  private readonly _listenGetDatabaseTableReply = (
-    ipcService: IpcService
-  ): void => {
-    ipcService.on('get-table-reply', (_: any, args: any) => {
-      // FIXME
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      this.ngZone.run(() => this._updateLocalTableData(args));
-    });
-  };
-  private readonly _updateLocalTableData = (
-    response: RequestTableResponse<T>
-  ) => {
-    this._getTableReply$.next(response);
-  };
-  private readonly _getDatabaseTable = (
-    tableName: TableName,
-    relations: TableName[] = [],
-    conditions: Where[] = [],
-    rawProperties: string[] = []
-  ): void => {
-    this.ipcService.send('get-table', { tableName, relations, conditions, rawProperties });
-  };
-  private readonly _getTableDataAsObservable = (
-    tableName: string
-  ): Observable<RequestTableResponse<T>> =>
-    this._getTableReply$.pipe(
-      filter(({ tableNameReply }) => tableNameReply === tableName)
-    );
-  private _deleteRowFromDatabaseTable = (
-    tableName: string,
-    ids: any[]
-  ): Promise<number> => {
-    return this.ipcService.invoke('delete-from-table', { tableName, ids });
-  };
-  private _addRowToTable = (
-    tableName: string,
-    element: T
-  ): Promise<number[] | null> => {
-    return this.ipcService.invoke('add-to-table', { tableName, element });
-  };
-  private _editTableRow = (tableName: string, element: T): Promise<number> => {
-    return this.ipcService.invoke('edit-from-table', { tableName, element });
-  };
-  private _getTableRow = (tableName: string, id: number): Promise<T> => {
-    return this.ipcService.invoke('get-table-row', { tableName, id });
-  };
 
   constructor(
     private readonly ipcService: IpcService,
@@ -87,7 +41,12 @@ export class DatabaseService<T> {
     },
     rawProperties: string[] = []
   ): void {
-    this._getDatabaseTable(tableName, options.relations, options.conditions, rawProperties);
+    this._getDatabaseTable(
+      tableName,
+      options.relations,
+      options.conditions,
+      rawProperties
+    );
   }
 
   getTableReply$(tableName: string): Observable<RequestTableResponse<T>> {
@@ -124,4 +83,63 @@ export class DatabaseService<T> {
   editElementFromTable$(tableName: string, element: T): Observable<number> {
     return from(this._editTableRow(tableName, element));
   }
+
+  private readonly _listenGetDatabaseTableReply = (
+    ipcService: IpcService
+  ): void => {
+    ipcService.on('get-table-reply', (_: any, args: any) => {
+      // FIXME
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      this.ngZone.run(() => this._updateLocalTableData(args));
+    });
+  };
+
+  private readonly _updateLocalTableData = (
+    response: RequestTableResponse<T>
+  ) => {
+    this._getTableReply$.next(response);
+  };
+
+  private readonly _getDatabaseTable = (
+    tableName: TableName,
+    relations: TableName[] = [],
+    conditions: Where[] = [],
+    rawProperties: string[] = []
+  ): void => {
+    this.ipcService.send('get-table', {
+      tableName,
+      relations,
+      conditions,
+      rawProperties,
+    });
+  };
+
+  private readonly _getTableDataAsObservable = (
+    tableName: string
+  ): Observable<RequestTableResponse<T>> =>
+    this._getTableReply$.pipe(
+      filter(({ tableNameReply }) => tableNameReply === tableName)
+    );
+
+  private _deleteRowFromDatabaseTable = (
+    tableName: string,
+    ids: any[]
+  ): Promise<number> => {
+    return this.ipcService.invoke('delete-from-table', { tableName, ids });
+  };
+
+  private _addRowToTable = (
+    tableName: string,
+    element: T
+  ): Promise<number[] | null> => {
+    return this.ipcService.invoke('add-to-table', { tableName, element });
+  };
+
+  private _editTableRow = (tableName: string, element: T): Promise<number> => {
+    return this.ipcService.invoke('edit-from-table', { tableName, element });
+  };
+
+  private _getTableRow = (tableName: string, id: number): Promise<T> => {
+    return this.ipcService.invoke('get-table-row', { tableName, id });
+  };
 }
