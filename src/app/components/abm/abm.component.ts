c979860d80ce81e8fc42552a8ab446411d608cda
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-02-25 12:25:52 -0300

    remover logicade seleccion del componente ABM

--- src/app/components/abm/abm.component.ts
+++ src/app/components/abm/abm.component.ts
@@ -1,12 +1,10 @@
 import {
-  AfterContentInit,
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
   Input,
   OnChanges,
   OnDestroy,
-  OnInit,
   Output,
   SimpleChanges,
   TemplateRef,
@@ -26,14 +24,13 @@ import { MessagesService } from '../../services/messages.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AbmComponent
-  implements OnInit, AfterContentInit, OnChanges, OnDestroy
+  implements OnChanges, OnDestroy
 {
   @Input() dataset: any[] = [];
   @Input() columns: AbmColum[] = [];
   @Input() paginator = true;
   @Input() detailFormValid = false;
   @Input() toolbar = true;
-  @Input() selectable = true;
   @Input() deleteButton = true;
   @Input() actionColumnStyleClass = 'w-8rem';
   @Input() abmDetailTemplate: TemplateRef<any> | null = null;
@@ -49,7 +46,6 @@ export class AbmComponent
   readonly rows = 5;
   readonly search: FormControl;
 
-  selected: any[] = [];
   detailDialogVisible = false;
 
   private readonly destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
@@ -59,68 +55,16 @@ export class AbmComponent
     private readonly messagesService: MessagesService
   ) {
     this.search = new FormControl('');
-    this.checkboxColumnMenuItems = [
-      {
-        label: 'Seleccionados',
-        items: [
-          {
-            label: 'Seleccionar página',
-            icon: PrimeIcons.CHECK_SQUARE,
-            command: () => this.selectPage(),
-          },
-          {
-            label: 'Anular selección',
-            icon: PrimeIcons.STOP,
-            command: () => this.clearSelected(),
-          },
-          { separator: true },
-          {
-            label: 'Eliminar elementos',
-            icon: PrimeIcons.TRASH,
-            command: () => this.deleteSelected(),
-            tooltipOptions: {
-              appendTo: 'body',
-              tooltipLabel: `Hasta un máximo de ${this.rows} por cada vez`,
-            },
-          },
-        ],
-      },
-    ];
     this.initFormValueChangeListeners();
   }
-
-  get deleteDisabled(): boolean {
-    return this.selected.length === 0;
-  }
-
-  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
-  ngOnInit() {}
-
-  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
-  ngAfterContentInit() {}
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['dataset']) {
       const { dataset } = changes;
       if (dataset.currentValue !== dataset.previousValue) {
-        this.clearSelected();
         this.closeDialog();
       }
     }
-  }
-
-  selectPage() {
-    this.selected = [
-      ...(this.primeNgTable?.dataToRender(null) as unknown as any[]),
-    ];
-  }
-
-  selectAll() {
-    this.selected = [...this.dataset];
-  }
-
-  clearSelected() {
-    this.selected = [];
   }
 
   clearSearch(): void {
@@ -129,32 +73,6 @@ export class AbmComponent
 
   filterByText(value: string): void {
     this.primeNgTable?.filterGlobal(value, 'contains');
-  }
-
-  deleteSelected() {
-    if (this.deleteDisabled) {
-      this.messagesService.warn('Seleccione elementos para eliminar');
-      return;
-    }
-    if (this.selected.length > this.rows) {
-      this.messagesService.warn(
-        `Máximo de elementos para eliminar ${this.rows} por vez`
-      );
-      return;
-    }
-
-    this.confirmationService.confirm({
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      message: `¿Eliminar los elementos seleccionados?<br>Cantidad de elementos seleccionados: ${this.selected.length}`,
-      header: 'Confirmar borrado',
-      icon: PrimeIcons.EXCLAMATION_TRIANGLE,
-      defaultFocus: 'reject',
-      acceptButtonStyleClass: 'p-button-danger',
-      accept: () => {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        this.deleteEvent.emit(this.selected.map((s) => s.id));
-      },
-    });
   }
 
   deleteElement(element: any) {
