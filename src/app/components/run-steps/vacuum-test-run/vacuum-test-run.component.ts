2a70201fe228dde881350ab9c29ae752218b1003
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-03-20 23:05:52 -0300

    se escriben los TODO

--- src/app/components/run-steps/vacuum-test-run/vacuum-test-run.component.ts
+++ src/app/components/run-steps/vacuum-test-run/vacuum-test-run.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@angular/core';
 import { VacuumTestStep } from '../../../models/business/interafces/steps/vacuum-step.model';
 import { CountTimerComponent } from '../../count-timer/count-timer.component';
+import { RunEssayService } from '../../../services/run-essay.service';
 
 @Component({
   selector: 'app-vacuum-test-run',
@@ -21,6 +22,8 @@ export class VacuumTestRunComponent implements OnChanges, AfterViewInit {
   @ViewChild('countTimer', { static: true }) countTimer!: CountTimerComponent;
 
   showCompleteName!: boolean;
+
+  constructor(private readonly runEssayService: RunEssayService) {}
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.currentStep) {
@@ -37,6 +40,10 @@ export class VacuumTestRunComponent implements OnChanges, AfterViewInit {
 
   timerStop(): void {
     // TODO mostrar resultados
+    // this.runEssayService
+    //   .getEssayStep(this.currentStep.id)
+    //   .get('executedStatus')
+    //   ?.setValue(StepStatus.Done);
   }
 
   private startTimer(): void {
