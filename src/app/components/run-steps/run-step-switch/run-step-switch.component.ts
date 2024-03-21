a155b7d6e180a9e905c34c1414a815b8d3f25b0e
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-03-20 21:34:26 -0300

    datos de la prueba de vacío

--- src/app/components/run-steps/run-step-switch/run-step-switch.component.ts
+++ src/app/components/run-steps/run-step-switch/run-step-switch.component.ts
@@ -1,11 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { EssayStep } from '../../../models/business/interafces/essay-step.model';
+import { Steps } from '../../../models/business/enums/steps.model';
 
 @Component({
   selector: 'app-run-step-switch',
   templateUrl: './run-step-switch.component.html',
   styleUrls: ['./run-step-switch.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RunStepSwitchComponent {
-  
+  @Input() currentStep!: EssayStep;
+
+  readonly Steps = Steps;
 }
--- src/app/components/run-steps/vacuum-test-run/vacuum-test-run.component.ts
+++ src/app/components/run-steps/vacuum-test-run/vacuum-test-run.component.ts
@@ -1,11 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { VacuumTestStep } from '../../../models/business/interafces/steps/vacuum-step.model';
 
 @Component({
   selector: 'app-vacuum-test-run',
   templateUrl: './vacuum-test-run.component.html',
   styleUrls: ['./vacuum-test-run.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VacuumTestRunComponent {
+export class VacuumTestRunComponent implements OnChanges {
+  @Input() currentStep!: VacuumTestStep;
 
+  showCompleteName!: boolean;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.currentStep) {
+      this.showCompleteName = this.getShowCompleteName(
+        changes.currentStep.currentValue as VacuumTestStep
+      );
+    }
+  }
+
+  private getShowCompleteName(currentStep: VacuumTestStep): boolean {
+    return currentStep.foreign.step?.name !== currentStep.form_control_raw.name;
+  }
 }
