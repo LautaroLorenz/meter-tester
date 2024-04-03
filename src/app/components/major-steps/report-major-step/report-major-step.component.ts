bdad59f5bdfb4d42cff99e32f7888d7a4f7a944c
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-04-02 22:00:24 -0300

    generación del reporte

--- src/app/components/major-steps/report-major-step/report-major-step.component.ts
+++ src/app/components/major-steps/report-major-step/report-major-step.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RunEssayService } from '../../../services/run-essay.service';
+import { EssayStep } from '../../../models/business/interafces/essay-step.model';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-report-major-step',
@@ -7,5 +10,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ReportMajorStepComponent {
+  executionSteps: EssayStep[] | undefined;
+  preparationStep: EssayStep | undefined;
+  
+  constructor(private readonly runEssayService: RunEssayService) {}
 
+  get executionSteps$(): Observable<EssayStep[]> {
+    return this.runEssayService.executionSteps$.pipe(
+      tap((executionSteps) => (this.executionSteps = executionSteps))
+    );
+  }
+
+  get preparationStep$(): Observable<EssayStep> {
+    return this.runEssayService.preparationStep$.pipe(
+      tap((preparationStep) => (this.preparationStep = preparationStep))
+    );
+  }
 }
