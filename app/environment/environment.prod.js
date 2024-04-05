cb054cafaf20ca4cf594f59258cdc131c60bc370
lautarolorenz <lautaro.lorenz@gmail.com>
2024-04-04 21:53:58 -0300

    configuraci{on del entorno

--- app/environment/environment.prod.js
+++ app/environment/environment.prod.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.APP_CONFIG = void 0;
 exports.APP_CONFIG = {
     production: true,
-    virtualMachine: true,
-    inspector: true,
+    virtualMachine: false,
+    inspector: false,
 };
 //# sourceMappingURL=environment.prod.js.map
