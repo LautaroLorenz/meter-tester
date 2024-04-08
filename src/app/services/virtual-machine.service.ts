173854ac21d72bce573686546622ac95c33cfe43
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-04-08 15:57:18 -0300

    remover import no usado

--- src/app/services/virtual-machine.service.ts
+++ src/app/services/virtual-machine.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { from, Observable, Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { IpcService } from './ipc.service';
 
 @Injectable({
