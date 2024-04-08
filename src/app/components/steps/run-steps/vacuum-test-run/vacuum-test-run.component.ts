cc4308c13c2ad959182c57a8c2d6ee1498b361ba
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-04-08 15:53:10 -0300

    se remueve casteos innecesarios

--- src/app/components/steps/run-steps/vacuum-test-run/vacuum-test-run.component.ts
+++ src/app/components/steps/run-steps/vacuum-test-run/vacuum-test-run.component.ts
@@ -122,7 +122,7 @@ export class VacuumTestRunComponent extends TestRunComponent {
           this.calculator.start$(
             this.getStepCalculatorBlocks(),
             this.preparationStep.form_control_raw,
-            this.currentStep.form_control_raw.meterConstant as MeterConstantEnum
+            this.currentStep.form_control_raw.meterConstant
           )
         ),
         // cambia el estado de los resultados
@@ -166,7 +166,7 @@ export class VacuumTestRunComponent extends TestRunComponent {
         .patchValue({
           resultStatus: this.calculateStandStatus(
             result.measuredPulses,
-            this.currentStep.form_control_raw.maxAllowedPulses as number
+            this.currentStep.form_control_raw.maxAllowedPulses
           ),
         });
     });
