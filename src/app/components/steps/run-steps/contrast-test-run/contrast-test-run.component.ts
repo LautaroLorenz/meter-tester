885726a9b3090f2db226b80eb869129ad56402aa
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-04-10 15:01:45 -0300

    parámetros ensayo de contraste

--- src/app/components/steps/run-steps/contrast-test-run/contrast-test-run.component.ts
+++ src/app/components/steps/run-steps/contrast-test-run/contrast-test-run.component.ts
@@ -1,11 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TestRunComponent } from '../../../../models/business/class/test-run-component.model';
+import {
+  ContrastTestEssayStep,
+  ContrastTestStandResult,
+} from '../../../../models/business/interafces/steps/contrast-test-step.model';
 
 @Component({
   selector: 'app-contrast-test-run',
   templateUrl: './contrast-test-run.component.html',
   styleUrls: ['./contrast-test-run.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContrastTestRunComponent {
-
+export class ContrastTestRunComponent extends TestRunComponent<ContrastTestEssayStep> {
+  // TODO
+  override isFailCondition(result: ContrastTestStandResult): boolean {
+    return false;
+  }
 }
--- src/app/components/steps/step-params/contrast-test-params/contrast-test-params.component.ts
+++ src/app/components/steps/step-params/contrast-test-params/contrast-test-params.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { StepParamsComponent } from '../../../../models/business/class/step-params-component.model';
+import { ContrastTestEssayStep } from '../../../../models/business/interafces/steps/contrast-test-step.model';
 
 @Component({
   selector: 'app-contrast-test-params',
   templateUrl: './contrast-test-params.component.html',
   styleUrls: ['./contrast-test-params.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContrastTestParamsComponent {
-
-}
+export class ContrastTestParamsComponent extends StepParamsComponent<ContrastTestEssayStep> {}
