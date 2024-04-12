3536482ebf9fd275c387a1620d55abc0fd9b63ca
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-04-12 19:00:49 -0300

    reporte ensayo de contraste

--- src/app/components/steps/result-report/contrast-test-pdf-report/contrast-test-pdf-report.component.ts
+++ src/app/components/steps/result-report/contrast-test-pdf-report/contrast-test-pdf-report.component.ts
@@ -1,11 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, forwardRef } from '@angular/core';
+import { PdfReportComponent } from '../../../../models/business/class/pdf-report-component.model';
+import { ContrastTestEssayStep, ContrastTestStandResult } from '../../../../models/business/interafces/steps/contrast-test-step.model';
+import { PreparationEssayStep } from '../../../../models/business/interafces/steps/preparation-step.model';
+import { TC_AlignHorizontal, TableColumn } from '../../../../models/core/table-column.model';
+import { StandStandResult } from '../../../../models/business/interafces/stand-result.model';
+import { Stand } from '../../../../models/business/interafces/stand.model';
 
 @Component({
   selector: 'app-contrast-test-pdf-report',
   templateUrl: './contrast-test-pdf-report.component.html',
   styleUrls: ['./contrast-test-pdf-report.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: PdfReportComponent,
+      useExisting: forwardRef(() => ContrastTestPdfReportComponent),
+    },
+  ],
 })
-export class ContrastTestPdfReportComponent {
+export class ContrastTestPdfReportComponent extends PdfReportComponent {
+  @Input() essayStep!: ContrastTestEssayStep;
+  @Input() preparationStep!: PreparationEssayStep;
 
+  readonly resultsColumn: TableColumn<StandStandResult> = {
+    alignHorizontal: TC_AlignHorizontal.Number,
+    header: 'Error [%]',
+    field: (item: StandStandResult): string => {
+      const realItem = item as Stand | ContrastTestStandResult;
+      return 'measuredError' in realItem
+        ? realItem.measuredError?.toString()
+        : '';
+    },
+  };
 }
