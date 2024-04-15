10615174ea8053cd17fe362cdcb248597f939f33
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-04-15 19:00:41 -0300

    ensayo completo de prueba

--- app/resources/database/seeds/D1_essay_templates.js
+++ app/resources/database/seeds/D1_essay_templates.js
@@ -1,9 +1,7 @@
-// const essays = ["Ensayo de contraste 1%", "Preparación"];
-
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-  // await knex("essay_templates").insert([{ id: 1, name: "Ensayo completo" }]);
+  await knex("essay_templates").insert([{ id: 1, name: "Completo" }]);
 };
--- app/resources/database/seeds/E1_essay_templates_steps.js
+++ app/resources/database/seeds/E1_essay_templates_steps.js
@@ -3,35 +3,34 @@
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-  // await knex("essay_templates_steps").insert([
-  //   {
-  //     id: 1,
-  //     order: 0,
-  //     essay_template_id: 1,
-  //     step_id: 1,
-  //     form_control_raw:
-  //       '[{"name":"Puesto 01","isActive":true,"meter":null,"serialNumber":null,"yearOfProduction":null},{"name":"Puesto 02","isActive":true,"meter":null,"serialNumber":null,"yearOfProduction":null},{"name":"Puesto 03","isActive":true,"meter":null,"serialNumber":null,"yearOfProduction":null},{"name":"Puesto 04","isActive":true,"meter":null,"serialNumber":null,"yearOfProduction":null},{"name":"Puesto 05","isActive":true,"meter":null,"serialNumber":null,"yearOfProduction":null},{"name":"Puesto 06","isActive":true,"meter":null,"serialNumber":null,"yearOfProduction":null},{"name":"Puesto 07","isActive":true,"meter":null,"serialNumber":null,"yearOfProduction":null},{"name":"Puesto 08","isActive":true,"meter":null,"serialNumber":null,"yearOfProduction":null},{"name":"Puesto 09","isActive":true,"meter":null,"serialNumber":null,"yearOfProduction":null},{"name":"Puesto 10","isActive":true,"meter":null,"serialNumber":null,"yearOfProduction":null}]',
-  //   },
-  //   {
-  //     id: 2,
-  //     order: 1,
-  //     essay_template_id: 1,
-  //     step_id: 2,
-  //     form_control_raw: `{"name":"11 impulsos por 5 minutos en Reactiva","meterConstant":1,"phaseL1":{"voltage":1},"phaseL2":{"voltage":2},"phaseL3":{"voltage":3},"maxAllowedPulses":11,"durationSeconds":300}`,
-  //   },
-  //   {
-  //     id: 3,
-  //     order: 2,
-  //     essay_template_id: 1,
-  //     step_id: 3,
-  //     form_control_raw: `{"name":"5% de error con 20 impulsos en Reactiva","meterConstant":1,"phaseL1": {"voltage":1,"current":4,"anglePhi":7},"phaseL2":{"voltage":2,"current":5,"anglePhi":8},"phaseL3":{"voltage":3,"current":6,"anglePhi":9},"maxAllowedError":5,"meterPulses":20}`,
-  //   },
-  //   {
-  //     id: 4,
-  //     order: 3,
-  //     essay_template_id: 1,
-  //     step_id: 4,
-  //     form_control_raw: `{"name":"entre 5 y 10 segundos con 20 imp Activa","meterConstant":0,"phaseL1": {"voltage":1,"current":4,"anglePhi":7},"phaseL2":{"voltage":2,"current":5,"anglePhi":8},"phaseL3":{"voltage":3,"current":6,"anglePhi":9},"allowedPulses":20,"minDurationSeconds":5,"maxDurationSeconds":10}`,
-  //   },
-  // ]);
+  await knex("essay_templates_steps").insert([
+    {
+      id: 1,
+      order: 0,
+      essay_template_id: 1,
+      step_id: 1,
+      form_control_raw: `[{"name":"Puesto 01","isActive":true,"meter":{"id":1,"model":"Gyr E330","maximumCurrent":16,"ratedCurrent":9,"ratedVoltage":186,"activeConstantValue":3230,"reactiveConstantValue":4919,"brand_id":2,"connection_id":2,"activeConstantUnit_id":1,"reactiveConstantUnit_id":1,"foreign":{"connection":{"id":2,"name":"Monofásico"},"brand":{"id":2,"name":"Actaris"},"activeConstantUnit":{"id":1,"name":"imp/kwh"},"reactiveConstantUnit":{"id":1,"name":"imp/kvarh"}},"label":"Actaris - Gyr E330"},"serialNumber":"111","yearOfProduction":2024},{"name":"Puesto 02","isActive":true,"meter":{"id":1,"model":"Gyr E330","maximumCurrent":16,"ratedCurrent":9,"ratedVoltage":186,"activeConstantValue":3230,"reactiveConstantValue":4919,"brand_id":2,"connection_id":2,"activeConstantUnit_id":1,"reactiveConstantUnit_id":1,"foreign":{"connection":{"id":2,"name":"Monofásico"},"brand":{"id":2,"name":"Actaris"},"activeConstantUnit":{"id":1,"name":"imp/kwh"},"reactiveConstantUnit":{"id":1,"name":"imp/kvarh"}},"label":"Actaris - Gyr E330"},"serialNumber":"222","yearOfProduction":2024},{"name":"Puesto 03","isActive":true,"meter":{"id":1,"model":"Gyr E330","maximumCurrent":16,"ratedCurrent":9,"ratedVoltage":186,"activeConstantValue":3230,"reactiveConstantValue":4919,"brand_id":2,"connection_id":2,"activeConstantUnit_id":1,"reactiveConstantUnit_id":1,"foreign":{"connection":{"id":2,"name":"Monofásico"},"brand":{"id":2,"name":"Actaris"},"activeConstantUnit":{"id":1,"name":"imp/kwh"},"reactiveConstantUnit":{"id":1,"name":"imp/kvarh"}},"label":"Actaris - Gyr E330"},"serialNumber":"333","yearOfProduction":2024},{"name":"Puesto 04","isActive":true,"meter":{"id":1,"model":"Gyr E330","maximumCurrent":16,"ratedCurrent":9,"ratedVoltage":186,"activeConstantValue":3230,"reactiveConstantValue":4919,"brand_id":2,"connection_id":2,"activeConstantUnit_id":1,"reactiveConstantUnit_id":1,"foreign":{"connection":{"id":2,"name":"Monofásico"},"brand":{"id":2,"name":"Actaris"},"activeConstantUnit":{"id":1,"name":"imp/kwh"},"reactiveConstantUnit":{"id":1,"name":"imp/kvarh"}},"label":"Actaris - Gyr E330"},"serialNumber":"444","yearOfProduction":2024},{"name":"Puesto 05","isActive":true,"meter":{"id":1,"model":"Gyr E330","maximumCurrent":16,"ratedCurrent":9,"ratedVoltage":186,"activeConstantValue":3230,"reactiveConstantValue":4919,"brand_id":2,"connection_id":2,"activeConstantUnit_id":1,"reactiveConstantUnit_id":1,"foreign":{"connection":{"id":2,"name":"Monofásico"},"brand":{"id":2,"name":"Actaris"},"activeConstantUnit":{"id":1,"name":"imp/kwh"},"reactiveConstantUnit":{"id":1,"name":"imp/kvarh"}},"label":"Actaris - Gyr E330"},"serialNumber":"555","yearOfProduction":2024},{"name":"Puesto 06","isActive":true,"meter":{"id":1,"model":"Gyr E330","maximumCurrent":16,"ratedCurrent":9,"ratedVoltage":186,"activeConstantValue":3230,"reactiveConstantValue":4919,"brand_id":2,"connection_id":2,"activeConstantUnit_id":1,"reactiveConstantUnit_id":1,"foreign":{"connection":{"id":2,"name":"Monofásico"},"brand":{"id":2,"name":"Actaris"},"activeConstantUnit":{"id":1,"name":"imp/kwh"},"reactiveConstantUnit":{"id":1,"name":"imp/kvarh"}},"label":"Actaris - Gyr E330"},"serialNumber":"666","yearOfProduction":2024},{"name":"Puesto 07","isActive":true,"meter":{"id":1,"model":"Gyr E330","maximumCurrent":16,"ratedCurrent":9,"ratedVoltage":186,"activeConstantValue":3230,"reactiveConstantValue":4919,"brand_id":2,"connection_id":2,"activeConstantUnit_id":1,"reactiveConstantUnit_id":1,"foreign":{"connection":{"id":2,"name":"Monofásico"},"brand":{"id":2,"name":"Actaris"},"activeConstantUnit":{"id":1,"name":"imp/kwh"},"reactiveConstantUnit":{"id":1,"name":"imp/kvarh"}},"label":"Actaris - Gyr E330"},"serialNumber":"777","yearOfProduction":2024},{"name":"Puesto 08","isActive":true,"meter":{"id":1,"model":"Gyr E330","maximumCurrent":16,"ratedCurrent":9,"ratedVoltage":186,"activeConstantValue":3230,"reactiveConstantValue":4919,"brand_id":2,"connection_id":2,"activeConstantUnit_id":1,"reactiveConstantUnit_id":1,"foreign":{"connection":{"id":2,"name":"Monofásico"},"brand":{"id":2,"name":"Actaris"},"activeConstantUnit":{"id":1,"name":"imp/kwh"},"reactiveConstantUnit":{"id":1,"name":"imp/kvarh"}},"label":"Actaris - Gyr E330"},"serialNumber":"888","yearOfProduction":2024},{"name":"Puesto 09","isActive":true,"meter":{"id":1,"model":"Gyr E330","maximumCurrent":16,"ratedCurrent":9,"ratedVoltage":186,"activeConstantValue":3230,"reactiveConstantValue":4919,"brand_id":2,"connection_id":2,"activeConstantUnit_id":1,"reactiveConstantUnit_id":1,"foreign":{"connection":{"id":2,"name":"Monofásico"},"brand":{"id":2,"name":"Actaris"},"activeConstantUnit":{"id":1,"name":"imp/kwh"},"reactiveConstantUnit":{"id":1,"name":"imp/kvarh"}},"label":"Actaris - Gyr E330"},"serialNumber":"999","yearOfProduction":2024},{"name":"Puesto 10","isActive":true,"meter":{"id":1,"model":"Gyr E330","maximumCurrent":16,"ratedCurrent":9,"ratedVoltage":186,"activeConstantValue":3230,"reactiveConstantValue":4919,"brand_id":2,"connection_id":2,"activeConstantUnit_id":1,"reactiveConstantUnit_id":1,"foreign":{"connection":{"id":2,"name":"Monofásico"},"brand":{"id":2,"name":"Actaris"},"activeConstantUnit":{"id":1,"name":"imp/kwh"},"reactiveConstantUnit":{"id":1,"name":"imp/kvarh"}},"label":"Actaris - Gyr E330"},"serialNumber":"000","yearOfProduction":2024}]`,
+    },
+    {
+      id: 2,
+      order: 1,
+      essay_template_id: 1,
+      step_id: 4,
+      form_control_raw: `{"name":"Arranque","meterConstant":0,"phaseL1":{"voltage":1,"current":4,"anglePhi":7},"phaseL2":{"voltage":2,"current":5,"anglePhi":8},"phaseL3":{"voltage":3,"current":6,"anglePhi":9},"allowedPulses":1,"minDurationSeconds":0,"maxDurationSeconds":0}`,
+    },
+    {
+      id: 3,
+      order: 2,
+      essay_template_id: 1,
+      step_id: 3,
+      form_control_raw: `{"name":"Contraste","meterConstant":0,"phaseL1":{"voltage":1,"current":4,"anglePhi":7},"phaseL2":{"voltage":2,"current":5,"anglePhi":8},"phaseL3":{"voltage":3,"current":6,"anglePhi":9},"maxAllowedError":1,"meterPulses":2}`,
+    },
+    {
+      id: 4,
+      order: 3,
+      essay_template_id: 1,
+      step_id: 2,
+      form_control_raw: `{"name":"Vacío","meterConstant":0,"phaseL1":{"voltage":1},"phaseL2":{"voltage":2},"phaseL3":{"voltage":3},"maxAllowedPulses":1,"durationSeconds":2}`,
+    },
+  ]);
 };
