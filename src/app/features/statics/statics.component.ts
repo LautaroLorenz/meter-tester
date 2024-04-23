e90eb721c7acde4a71e750e37c78d8e49cd0c9fb
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-04-23 09:29:25 -0300

    título página estadísticas

--- src/app/features/statics/statics.component.ts
+++ src/app/features/statics/statics.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './statics.component.html',
-  styleUrls: ['./statics.component.scss']
+  styleUrls: ['./statics.component.scss'],
 })
 export class StaticsComponent {
-
+  readonly title = 'Estadísticas';
 }
