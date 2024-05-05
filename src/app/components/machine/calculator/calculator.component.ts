9b3c8bf6bffd956261da7f6d4d2aeeb66ac7caad
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-05-05 10:10:25 -0300

    actualización de comandos calculador

--- src/app/components/machine/calculator/calculator.component.ts
+++ src/app/components/machine/calculator/calculator.component.ts
@@ -7,13 +7,16 @@ import {
 import { MachineDeviceComponent } from '../../../models/business/class/machine-device.model';
 import { Devices } from '../../../models/business/enums/devices.model';
 import { SoftwareCalculatorCommands } from '../../../models/business/enums/commands.model';
-import { Observable, map, of, switchMap, tap } from 'rxjs';
+import { Observable, map, tap, from, toArray, concatMap } from 'rxjs';
 import { Stand } from '../../../models/business/interafces/stand.model';
-import { MeterConstantEnum } from '../../../models/business/constants/meter-constant.model';
+import {
+  MeterConstantEnum,
+  MeterConstantUnitEnum,
+} from '../../../models/business/constants/meter-constant.model';
 import { DeviceStatus } from '../../../models/business/enums/device-status.model';
 import { StandMeterConstantPipe } from '../../../pipes/business/stand-meter-constant.pipe';
-import { APP_CONFIG } from '../../../../environments/environment';
 import { CommandDirector } from '../../../models/business/class/command-director.model';
+import { ActiveStand } from '../../../models/business/interafces/active-stand.model';
 
 @Component({
   selector: 'app-calculator',
@@ -28,100 +31,152 @@ export class CalculatorComponent extends MachineDeviceComponent {
 
   readonly standMeterConstantPipe = inject(StandMeterConstantPipe);
 
-  stop$(): Observable<string> {
-    return of(this.buildCommand(SoftwareCalculatorCommands.STOP)).pipe(
-      tap(() => this.deviceStatus$.next(DeviceStatus.StopInProgress)),
-      switchMap((stopCommand) => this.write$(stopCommand)),
+  stop$(activeStands: ActiveStand[]): Observable<string[]> {
+    const observables = activeStands.map(({ index }) => {
+      const standNumber = (index + 1).toString().padStart(2, '0');
+      const standBlock = `P${standNumber}`;
+      const command = this.buildCommand(
+        standBlock,
+        SoftwareCalculatorCommands.STOP
+      );
+      return this.write$(command, () =>
+        this.messagesService.error(
+          `Error de comunicación puesto [${standNumber}]`
+        )
+      );
+    });
+    this.deviceStatus$.next(DeviceStatus.StopInProgress);
+    return from(observables).pipe(
+      concatMap((obs) => obs),
+      toArray(),
       tap(() => this.deviceStatus$.next(DeviceStatus.Stopped))
     );
   }
 
-  start$(
-    stepParamBlocks: string[],
-    preparationStepStands: Stand[],
+  resultsTS01$(
+    activeStands: ActiveStand[],
+    patternConstant: number,
+    stepMeterPulses: number,
     stepMeterConstant: MeterConstantEnum
-  ): Observable<string> {
-    // para los stands que tiene la máquina completamos el comando según puesto activo/inactivo
-    const standBlocks: string[] = preparationStepStands.map(
-      (stand, index) =>
-        `${CommandDirector.STAND}${this.subBlockStandIndex(index)}${this.subBlockStandConstant(
-          stand,
-          stepMeterConstant
-        )}`
-    );
-
-    // cunado la máquina tiene menos stands que los de la logitud del comando
-    // completamos la longitud del comando con puestos apagados.
-    let padBlockQuantity =
-      APP_CONFIG.commandStandsQuantity - standBlocks.length;
-    const padStandBlocks = [];
-    while (padBlockQuantity > 0) {
-      const index = APP_CONFIG.commandStandsQuantity - padBlockQuantity;
-      padStandBlocks.push(
-        `${CommandDirector.STAND}${this.subBlockStandIndex(index)}${this.subBlockStandConstant()}`
+  ): Observable<number[]> {
+    const observables = activeStands.map((activeStand) => {
+      const standNumber = (activeStand.index + 1).toString().padStart(2, '0');
+      const standBlock = `P${standNumber}`;
+      const pattern = patternConstant.toString().padStart(10, '0');
+      const pulses = stepMeterPulses.toString().padStart(5, '0');
+      const meterConstant = this.getMeterConstantBlock(
+        stepMeterConstant,
+        activeStand.stand
       );
-      padBlockQuantity--;
-    }
-
-    return of(
-      this.buildCommand(...stepParamBlocks, ...standBlocks, ...padStandBlocks)
-    ).pipe(
-      tap(() => this.deviceStatus$.next(DeviceStatus.StartInProgress)),
-      switchMap((startCommand) => this.write$(startCommand)),
-      tap(() => this.deviceStatus$.next(DeviceStatus.Working))
+      const command = this.buildCommand(
+        standBlock,
+        SoftwareCalculatorCommands.RESULT_TS01,
+        pattern,
+        pulses,
+        meterConstant
+      );
+      return this.write$(command, () =>
+        this.messagesService.error(
+          `Error de comunicación puesto [${standNumber}]`
+        )
+      );
+    });
+    this.deviceStatus$.next(DeviceStatus.Working);
+    return from(observables).pipe(
+      concatMap((obs) => obs),
+      toArray(),
+      map((responses) => this.mapResultsCommand(responses))
     );
   }
 
-  loopResults$(): Observable<number[]> {
-    return this.loopWrite$(
-      this.buildCommand(SoftwareCalculatorCommands.RESULTS),
-      this.loopDelay,
-      () => this.deviceStatus$.value === DeviceStatus.Working
-    ).pipe(map((response) => this.mapResultsCommand(response)));
+  resultsTS02$(activeStands: ActiveStand[]): Observable<number[]> {
+    const observables = activeStands.map((activeStand) => {
+      const standNumber = (activeStand.index + 1).toString().padStart(2, '0');
+      const standBlock = `P${standNumber}`;
+      const command = this.buildCommand(
+        standBlock,
+        SoftwareCalculatorCommands.RESULT_TS02
+      );
+      return this.write$(command, () =>
+        this.messagesService.error(
+          `Error de comunicación puesto [${standNumber}]`
+        )
+      );
+    });
+    this.deviceStatus$.next(DeviceStatus.Working);
+    return from(observables).pipe(
+      concatMap((obs) => obs),
+      toArray(),
+      map((responses) => this.mapResultsCommand(responses))
+    );
   }
 
-  private mapResultsCommand(command: string): number[] {
-    const blocks = CommandDirector.getBlocks(command);
-    const allResultsBlock = blocks.filter((block) => block.includes(CommandDirector.STAND));
-    const resultsBlock = allResultsBlock.filter(
-      (_, index) => index < APP_CONFIG.standsQuantiy
-    );
-    return resultsBlock.map((block) => {
-      let result = block.substring(4);
-      const signal = block.substring(4, 5);
-      let value: number;
-      if (signal === '-' || signal === '+') {
-        result = block.substring(5);
-        if (signal === '+') {
-          value = Number(result);
-        } else {
-          value = -1 * Number(result);
-        }
-      } else {
-        value = Number(result);
+  private mapResultsCommand(commands: string[]): number[] {
+    return commands.map((command) => {
+      const blocks = CommandDirector.getBlocks(command);
+      const resultBlock = blocks[4];
+      let resultValue = Number(resultBlock.substring(4));
+      const resultSignal = resultBlock.substring(3, 4);
+      if (resultSignal === '-') {
+        resultValue = resultValue * -1;
       }
+
       const decimals = Math.pow(10, this.resultDecimalsQuantity);
-      return Math.round((value / decimals) * decimals) / decimals;
+      return Math.round((resultValue / decimals) * decimals) / decimals;
     });
   }
 
-  private subBlockStandIndex(index: number) {
-    return (index + 1).toString().padStart(2, '0');
+  private getMeterConstantBlock(
+    stepMeterConstant: MeterConstantEnum,
+    stand: Stand
+  ): string {
+    const getStartChar = (
+      standConstantUnit: MeterConstantUnitEnum
+    ): 'I' | 'W' => {
+      switch (standConstantUnit) {
+        case MeterConstantUnitEnum.impKvarh:
+        case MeterConstantUnitEnum.impKwh:
+          return 'I';
+        case MeterConstantUnitEnum.varhImp:
+        case MeterConstantUnitEnum.whImp:
+          return 'W';
+      }
+    };
+    const meterConstantUnit = this.standMeterConstantPipe.transform(
+      stepMeterConstant,
+      stand.meter,
+      'OnlyUnit'
+    );
+    const meterConstantValue = this.standMeterConstantPipe.transform(
+      stepMeterConstant,
+      stand.meter,
+      'OnlyValue'
+    );
+    const startChart = getStartChar(meterConstantUnit as MeterConstantUnitEnum);
+    let value = '';
+    // 7 dígitos enteros
+    if (startChart === 'I') {
+      value = meterConstantValue.padStart(7, '0');
+    }
+    // 3 enteros y 4 decimales
+    if (startChart === 'W') {
+      value = this.formatNumberWithPadding(meterConstantValue);
+    }
+    return `${startChart}${value}`;
   }
 
-  private subBlockStandConstant(
-    stand?: Stand,
-    stepMeterConstant?: MeterConstantEnum
-  ): string {
-    const offStandConstant = 'xxxxx';
-    if (!stand || !stepMeterConstant) {
-      return offStandConstant;
-    }
-    if (!stand.isActive) {
-      return offStandConstant;
-    }
-    return this.standMeterConstantPipe
-      .transform(stepMeterConstant, stand.meter, 'OnlyValue')
-      .padStart(offStandConstant.length, '0');
+  // 3 enteros y 4 decimales
+  private formatNumberWithPadding(meterConstantValue: string): string {
+    // Separar la parte entera y decimal
+    const parts = meterConstantValue.split('.');
+    let integerPart = parts[0];
+    let decimalPart = parts[1] || '0';
+
+    // Rellenar con ceros a la izquierda
+    integerPart = integerPart.padStart(3, '0');
+    decimalPart = decimalPart.padEnd(4, '0');
+
+    // Concatenar y devolver el resultado
+    return integerPart + decimalPart;
   }
 }
