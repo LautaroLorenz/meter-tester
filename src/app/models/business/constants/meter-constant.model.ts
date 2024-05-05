049b3e7da88954fd1ba3d19cc0e6f6490155e6ba
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-05-05 10:12:20 -0300

    nuevos enums para unidades

--- src/app/models/business/constants/meter-constant.model.ts
+++ src/app/models/business/constants/meter-constant.model.ts
@@ -8,6 +8,23 @@ export enum MeterConstantEnum {
   Reactive = 1,
 }
 
+export enum MeterConstantUnitEnum {
+  whImp = 'wh/imp',
+  impKwh = 'imp/kwh',
+  varhImp = 'varh/imp',
+  impKvarh = 'imp/kvarh',
+}
+
+export enum ActiveConstantUnitEnum {
+  impKwh = 1,
+  whImp = 2,
+}
+
+export enum ReactiveConstantUnitEnum {
+  impKvarh = 1,
+  varhImp = 2,
+}
+
 export const MeterConstants: MeterConstant[] = [
   {
     id: MeterConstantEnum.Active,
