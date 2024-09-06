58d82a0499ce3daa9cdff49acb2ce4ab1f437e85
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2024-09-05 21:01:37 -0300

    poder conectar con una base de datos diferente

--- app/resources/database/database.js
+++ app/resources/database/database.js
@@ -12,10 +12,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const electron_1 = require("electron");
 const knexfile_1 = require("./knexfile");
 const databasePath_1 = require("./databasePath");
+const Store = require("electron-store");
+const store = new Store();
 let knex;
 let created = false;
 let updated = false;
 let updatedError = false;
+let mainWindow;
 function runSeedsFirstTime(knex) {
     return __awaiter(this, void 0, void 0, function* () {
         // Verifica si la base de datos está vacía
@@ -30,25 +33,28 @@ function runSeedsFirstTime(knex) {
         }
     });
 }
+function doConnection() {
+    knex = require('knex')(knexfile_1.default);
+    // Actualizar base de datos
+    knex.migrate
+        .latest()
+        .then((migrations) => {
+        var _a;
+        if (((_a = migrations === null || migrations === void 0 ? void 0 : migrations[1]) === null || _a === void 0 ? void 0 : _a.length) > 0) {
+            console.log(migrations);
+            updated = true;
+        }
+        // Run seeds first time
+        runSeedsFirstTime(knex);
+    })
+        .catch(() => {
+        updatedError = true;
+    });
+    return knex;
+}
 exports.default = {
     connect: () => {
-        knex = require('knex')(knexfile_1.default);
-        // Actualizar base de datos
-        knex.migrate
-            .latest()
-            .then((migrations) => {
-            var _a;
-            if (((_a = migrations === null || migrations === void 0 ? void 0 : migrations[1]) === null || _a === void 0 ? void 0 : _a.length) > 0) {
-                console.log(migrations);
-                updated = true;
-            }
-            // Run seeds first time
-            runSeedsFirstTime(knex);
-        })
-            .catch(() => {
-            updatedError = true;
-        });
-        return knex;
+        return doConnection();
     },
     register: () => {
         electron_1.ipcMain.handle('get-database-path', () => __awaiter(void 0, void 0, void 0, function* () {
@@ -78,6 +84,43 @@ exports.default = {
             }, 100);
             return response;
         }));
+        // seleccionar la base de datos a la que se realizará la conexión
+        electron_1.ipcMain.handle('change-connection-path', () => __awaiter(void 0, void 0, void 0, function* () {
+            if (!mainWindow) {
+                return null;
+            }
+            const result = yield electron_1.dialog.showOpenDialog(mainWindow, {
+                title: 'Seleccionar archivo de base de datos',
+                buttonLabel: 'Seleccionar',
+                properties: ['openFile'],
+                filters: [{ name: 'Base de datos', extensions: ['db'] }]
+            });
+            if (result.canceled || result.filePaths.length === 0) {
+                return { success: false, message: 'No file selected' };
+            }
+            const dataBasePath = result.filePaths[0];
+            try {
+                // Reconectar con la nueva base de datos
+                knexfile_1.default.connection = dataBasePath;
+                doConnection();
+                // Hacer una query para ver si la base de datos está ok
+                const isEmpty = yield knex('steps')
+                    .count('* as count')
+                    .then((rows) => rows[0].count === 0);
+                if (isEmpty) {
+                    return { success: false, message: 'Connection failed: Not valid database' };
+                }
+                // Guardar el path de conexión en el store
+                store.set('dbDirectory', dataBasePath);
+                return { success: true, message: 'Database connected successfully' };
+            }
+            catch (error) {
+                return { success: false, message: `Connection failed: ${error === null || error === void 0 ? void 0 : error.message}` };
+            }
+        }));
     },
+    setMainWindow: (mainWindowParam) => {
+        mainWindow = mainWindowParam;
+    }
 };
 //# sourceMappingURL=database.js.map
--- src/app/components/database-settings-dialog/database-settings-dialog.component.ts
+++ src/app/components/database-settings-dialog/database-settings-dialog.component.ts
@@ -1,4 +1,11 @@
+import { RestartService } from './../../services/restart.service';
+import { tap, finalize } from 'rxjs';
+import { DatabaseService } from './../../services/database.service';
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { MessagesService } from '../../services/messages.service';
+import { BlockUIService } from '../../services/block-ui.service';
+import { ConfirmationService, PrimeIcons } from 'primeng/api';
+
 
 @Component({
   selector: 'app-database-settings-dialog',
@@ -10,11 +17,46 @@ export class DatabaseSettingsDialogComponent {
   @Input() display = false;
   @Output() displayChange = new EventEmitter<boolean>();
 
+  constructor(
+    private blockUIService: BlockUIService,
+    private databaseService: DatabaseService<any>,
+    private messagesService: MessagesService,
+    private confirmationService: ConfirmationService,
+    private restartService: RestartService,
+  ) { }
+
   onHide(): void {
     this.displayChange.emit(this.display);
   }
 
   changeDataBaseConnection(): void {
-    // TODO
+    this.blockUIService.setBlocked(true);
+    this.databaseService.changeConnectionPath().pipe(
+      tap(({ success, message }) => {
+        if (success) {
+          this.messagesService.success('Conexión con la base de datos modificada');
+          this.confirmationService.confirm({
+            message: 'Para aplicar los cambios es necesario reiniciar.',
+            header: 'Reiniciar aplicación',
+            icon: PrimeIcons.EXCLAMATION_TRIANGLE,
+            defaultFocus: 'accept',
+            acceptButtonStyleClass: 'p-button-success',
+            acceptLabel: 'Reiniciar',
+            accept: () => {
+              this.restartService.restartApp().subscribe();
+            },
+            rejectVisible: false,
+            closeOnEscape: false
+          });
+        } else if (message) {
+          this.messagesService.error(message);
+          this.blockUIService.setBlocked(false);
+        }
+      }),
+      finalize(() => {
+        this.display = false;
+        this.onHide();
+      })
+    ).subscribe()
   }
 }
