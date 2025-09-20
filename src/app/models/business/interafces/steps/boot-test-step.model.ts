106724405ede171a40735c6e39323e12d9459f7c
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2025-09-20 14:43:14 -0300

    factor de potencia entre -1 y 1

--- src/app/models/business/interafces/steps/boot-test-step.model.ts
+++ src/app/models/business/interafces/steps/boot-test-step.model.ts
@@ -52,21 +52,21 @@ export class BootTestFormBuilder extends AbstractStepFormBuilder<BootTestStep, B
                     voltage: [undefined, [Validators.required.bind(this), Validators.min(0), Validators.max(500)]],
                     current: [undefined, [Validators.required.bind(this), Validators.min(0), Validators.max(200)]],
                     anglePhi: [undefined, [Validators.min(0), Validators.max(359.9)]],
-                    powerFactor: [undefined, [Validators.required.bind(this), Validators.min(0), Validators.max(1)]],
+                    powerFactor: [undefined, [Validators.required.bind(this), Validators.min(-1), Validators.max(1)]],
                     powerFactorLetter: [undefined, [Validators.required.bind(this)]]
                 }),
                 phaseL2: fb.nonNullable.group({
                     voltage: [undefined, [Validators.required.bind(this), Validators.min(0), Validators.max(500)]],
                     current: [undefined, [Validators.required.bind(this), Validators.min(0), Validators.max(200)]],
                     anglePhi: [undefined, [Validators.min(0), Validators.max(359.9)]],
-                    powerFactor: [undefined, [Validators.required.bind(this), Validators.min(0), Validators.max(1)]],
+                    powerFactor: [undefined, [Validators.required.bind(this), Validators.min(-1), Validators.max(1)]],
                     powerFactorLetter: [undefined, [Validators.required.bind(this)]]
                 }),
                 phaseL3: fb.nonNullable.group({
                     voltage: [undefined, [Validators.required.bind(this), Validators.min(0), Validators.max(500)]],
                     current: [undefined, [Validators.required.bind(this), Validators.min(0), Validators.max(200)]],
                     anglePhi: [undefined, [Validators.min(0), Validators.max(359.9)]],
-                    powerFactor: [undefined, [Validators.required.bind(this), Validators.min(0), Validators.max(1)]],
+                    powerFactor: [undefined, [Validators.required.bind(this), Validators.min(-1), Validators.max(1)]],
                     powerFactorLetter: [undefined, [Validators.required.bind(this)]]
                 }),
                 allowedPulses: [undefined, [Validators.required.bind(this), Validators.min(0), Validators.max(99)]],
