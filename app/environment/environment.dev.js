9b8846927eda0c45a700b05feab36a7bec56fb60
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2025-09-22 18:25:16 -0300

    udpate environement

--- app/environment/environment.dev.js
+++ app/environment/environment.dev.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.APP_CONFIG = void 0;
 exports.APP_CONFIG = {
     production: false,
-    virtualMachine: true,
-    inspector: true,
+    virtualMachine: false,
+    inspector: true
 };
 //# sourceMappingURL=environment.dev.js.map
