b6319ea957e7ff33cb7e6a9984658bf7fc80bf65
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2025-09-28 15:05:28 -0300

    commadn size info

--- app/resources/machine/command-size.js
+++ app/resources/machine/command-size.js
@@ -11,19 +11,22 @@ exports.CommandsSizes = [
         // Patrón -> Software - Respuesta - Constante y Valores medidos de fases
         pattern: `B\\|PS`,
         size: 41,
-        dividerPositions: [1, 4, 9, 12, 15, 18, 21, 24, 27, 31, 35, 39]
+        dividerPositions: [1, 4, 9, 12, 15, 18, 21, 24, 27, 31, 35, 39],
+        patternLength: 4
     },
     {
         // Generador -> Software - Respuesta - ACK
         pattern: `B\\|GS`,
         size: 8,
-        dividerPositions: [1, 4, 6]
+        dividerPositions: [1, 4, 6],
+        patternLength: 4
     },
     {
         // Calculador -> Software - Respuesta - Puesto (1-255 -> regex "[\\s\\S]") + ACK + Valores del ensayo
         pattern: `B\\|CS\\|[\\s\\S]\\|`,
         size: 12,
-        dividerPositions: [1, 4, 6, 10]
+        dividerPositions: [1, 4, 6, 10],
+        patternLength: 7
     }
 ];
 //# sourceMappingURL=command-size.js.map
