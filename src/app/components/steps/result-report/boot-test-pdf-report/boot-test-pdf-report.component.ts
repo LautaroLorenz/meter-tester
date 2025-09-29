53a923ebb4fac22d76bfebd56618d09b5024e3cf
jim-lautaro-cargoproduce <lautaro@cargoproduce.com>
2025-09-28 22:44:31 -0300

    fix report pdf

--- src/app/components/steps/result-report/boot-test-pdf-report/boot-test-pdf-report.component.ts
+++ src/app/components/steps/result-report/boot-test-pdf-report/boot-test-pdf-report.component.ts
@@ -8,6 +8,7 @@ import { PreparationEssayStep } from '../../../../models/business/interafces/ste
 import { StandStandResult } from '../../../../models/business/interafces/stand-result.model';
 import { TC_AlignHorizontal, TableColumn } from '../../../../models/core/table-column.model';
 import { Stand } from '../../../../models/business/interafces/stand.model';
+import { formatHeaderWithUnits } from '../../../../utils/table-utils';
 
 @Component({
     selector: 'app-boot-test-pdf-report',
@@ -27,7 +28,7 @@ export class BootTestPdfReportComponent extends PdfReportComponent {
 
     readonly resultsColumn: TableColumn<StandStandResult> = {
         alignHorizontal: TC_AlignHorizontal.Number,
-        header: 'Impulsos',
+        header: formatHeaderWithUnits('Impulsos'),
         field: (item: StandStandResult): string => {
             const realItem = item as Stand | BootTestStandResult;
             return 'measuredPulses' in realItem ? realItem.measuredPulses?.toString() : '';
